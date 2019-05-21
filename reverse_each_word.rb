141b157e42c718de6222387744d2ae7c6490acc6
Margot Konig <margot.konig@blackrock.com>
2019-05-21 16:19:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,6 @@
 def reverse_each_word(string)
   new_arr = string.split(" ")
-  return_array = []
-  new_arr.each do |string|
+  new_arr.collect do |string|
     return_array << string.reverse
   end
-  return_array.join (" ")
 end
